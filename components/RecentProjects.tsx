b15a8d282a9bc3c0c5c4698d8b69807d3a72b989
egyramadhan <egyramadhan3@gmail.com>
2024-07-14 22:03:54 +0700

    chore: Update favicon and element IDs for consistency

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from "@/data";
 import { PinContainer } from "./ui/3d-pin";
 import { FaLocationArrow } from "react-icons/fa";
+import Link from "next/link";
 
 const RecentProjects = () => {
   return (
@@ -14,7 +15,6 @@ const RecentProjects = () => {
           <div
             className="sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
             key={id}
-            
           >
             <PinContainer title={title} href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
@@ -44,12 +44,16 @@ const RecentProjects = () => {
                     </div>
                   ))}
                 </div>
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
+                {link && (
+                  <div className="flex justify-center items-center">
+                    <Link href={link} target="_blank">
+                      <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                        Check Live Site
+                      </p>
+                    </Link>
+                    <FaLocationArrow className="ms-3" color="#CBACF9" />
+                  </div>
+                )}
               </div>
             </PinContainer>
           </div>
